8def4dbffb557e6e3b049dafaf6b1d35342060a8
hu4nana <112612929+hu4nana@users.noreply.github.com>
2023-11-01 23:18:42 +0900

    Add Blend Tree of Manuka in the AttackTest Scene

--- CapstoneProject/Assets/_3DArt/Test/BlenderTreeTest/TestPlayer.cs
+++ CapstoneProject/Assets/_3DArt/Test/BlenderTreeTest/TestPlayer.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class TestPlayer : MonoBehaviour
 {
     Animator ani;
+    float dir;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ani=GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (InputManager.GetHorizontal() != 0)
+        {
+            dir = InputManager.GetHorizontal();
+            Debug.Log(dir);
+            ani.SetFloat("Blend", -dir);
+            ani.SetBool("isWalk", true);
+        }
+        else
+        {
+            ani.SetBool("isWalk", false);
+        }
+        ani.SetFloat("Blend", dir);
+        ani.SetFloat("xDir", InputManager.GetMove().x);
+        ani.SetFloat("yDir", InputManager.GetMove().y);
     }
 }
