1e344b5b61999383034f8e0e6dce0c7ed8802d59
hu4nana <112612929+hu4nana@users.noreply.github.com>
2023-11-03 00:16:42 +0900

    Animation....

--- CapstoneProject/Assets/_3DArt/Test/BlenderTreeTest/TestPlayer.cs
+++ CapstoneProject/Assets/_3DArt/Test/BlenderTreeTest/TestPlayer.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 public class TestPlayer : MonoBehaviour
 {
     Animator ani;
+    Rigidbody rigid;
     float dir;
 
     // Start is called before the first frame update
     void Start()
     {
         ani=GetComponentInChildren<Animator>();
+        rigid=GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (InputManager.GetHorizontal() != 0)
+        PlayerMove();
+
+    }
+
+
+    void PlayerMove()
+    {
+        //if (Input.GetKeyDown(KeyCode.LeftArrow)||
+        //    Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+        //    if(InputManager.GetHorizontal()!=dir)
+        //    {
+        //        ani.SetBool("isTurn", true);
+        //    }
+        //    Debug.Log("KeyDown");
+        //}
+        if (Input.GetKey(KeyCode.LeftArrow) ||
+            Input.GetKey(KeyCode.RightArrow))
         {
-            dir = InputManager.GetHorizontal();
-            Debug.Log(dir);
-            ani.SetFloat("Blend", -dir);
             ani.SetBool("isWalk", true);
+            Debug.Log("Key");
         }
-        else
+        if (Input.GetKeyUp(KeyCode.LeftArrow) ||
+            Input.GetKeyUp(KeyCode.RightArrow))
         {
             ani.SetBool("isWalk", false);
+            Debug.Log("KeyUp");
         }
-        ani.SetFloat("Blend", dir);
-        ani.SetFloat("xDir", InputManager.GetMove().x);
-        ani.SetFloat("yDir", InputManager.GetMove().y);
+        if (InputManager.GetHorizontal() != 0)
+        {
+            Quaternion targetrotation = Quaternion.LookRotation(
+                Vector3.right * InputManager.GetHorizontal());
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetrotation, Time.deltaTime * 90);
+        }
+        
+        ////ani.SetFloat("xDir", InputManager.GetMove().x);
+        ////ani.SetFloat("yDir", InputManager.GetMove().y);
+        //ani.SetFloat("Blend", dir);
     }
 }
